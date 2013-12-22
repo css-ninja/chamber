04d150861da45dcd4e065b0114863bdd822b4a24
David Leuliette <dleuliette@gmail.com>
2013-12-22 21:14:05 +0100

    Add javascript for maps & settings for contact

--- config.rb
+++ config.rb
@@ -1,10 +1,5 @@
-compass_config do |config|
-  config.output_style = :compact
-end
-
 require 'slim'
 activate :livereload
-activate :directory_indexes
 
 set :js_dir, 'assets/javascripts'
 set :css_dir, 'assets/stylesheets'
